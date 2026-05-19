767e50cb23f2b40eda35cb6dbf85f9e26d351c3c
SaudAlrashid <saud.alrashid17@gmail.com>
2026-05-19 19:21:24 +0300

    m

--- IT/Allpage.php
+++ IT/Allpage.php
@@ -1,3 +1,8 @@
+<?php
+session_start();
+
+include 'cafes.php';
+?>
 <!DOCTYPE html>
 <html lang="en">
   <head>
---
+++ IT/cafes.php
@@ -0,0 +1,62 @@
+<?php
+
+$cafes = [
+
+  [
+    "name" => "MKTH",
+    "img" => "./Coffee Pages/Pics/mkth.png",
+    "rating" => "⭐ 4.6 Study Friendly",
+    "tags" => ["WiFi", "Quiet"],
+    "link" => "./Coffee Pages/cafe-mkth.html"
+  ],
+
+  [
+    "name" => "Peacock",
+    "img" => "./Coffee Pages/Pics/Peacock.jpg",
+    "rating" => "⭐ 4.8 Specialty",
+    "tags" => ["Private", "Outlets"],
+    "link" => "./Coffee Pages/cafe-peacock.html"
+  ],
+
+  [
+    "name" => "Iota",
+    "img" => "./Coffee Pages/Pics/iota.jpg",
+    "rating" => "⭐ 4.5 Minimal",
+    "tags" => ["Study", "Focus"],
+    "link" => "./Coffee Pages/cafe-iota.html"
+  ],
+
+  [
+    "name" => "Kultura",
+    "img" => "./Coffee Pages/Pics/kaltura.png",
+    "rating" => "⭐ 4.7 Premium",
+    "tags" => ["Calm", "Private"],
+    "link" => "./Coffee Pages/cafe-kultura.html"
+  ],
+
+  [
+    "name" => "Ashjar",
+    "img" => "./Coffee Pages/Pics/Ashjar.png",
+    "rating" => "⭐ 4.7 Aesthetic",
+    "tags" => ["Study", "WiFi"],
+    "link" => "./Coffee Pages/cafe-ashjar.html"
+  ],
+
+  [
+    "name" => "Eventhu",
+    "img" => "./Coffee Pages/Pics/Eventhu.jpg",
+    "rating" => "⭐ 4.5 Cozy",
+    "tags" => ["Focus", "Coffee"],
+    "link" => "./Coffee Pages/cafe-eventhu.html"
+  ],
+
+  [
+    "name" => "Camel Step",
+    "img" => "./Coffee Pages/Pics/Camelstep.png",
+    "rating" => "⭐ 4.8 Specialty",
+    "tags" => ["Quiet", "Power"],
+    "link" => "./Coffee Pages/cafe-camelstep.html"
+  ]
+
+];
+?>
